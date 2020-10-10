cb97b9fe04e4887301f36548585f0bd98f33ed40
zagjade <zagjade@163.com>
2020-10-10 10:52:40 +0800

    Optimize installation scripts

--- install.sh
+++ install.sh
@@ -8,10 +8,19 @@
 
 set -e
 
+SCRIPT_DIR=$(cd `dirname $0`; pwd)
+
+. ${SCRIPT_DIR}/setenv.sh
+
 VERSION=1.0.1
+TON_BINARY_FILE=toncommunity-binary-v${VERSION}.tar.gz
 
-wget https://github.com/TONCommunity/ton/releases/download/v${VERSION}/toncommunity-binary-v${VERSION}.tar.gz
+wget https://github.com/TONCommunity/ton/releases/download/v1.0.1/$TON_BINARY_FILE
 
-tar xzf toncommunity-binary-v${VERSION}.tar.gz -C .
+mkdir -p ${TON_WORK_DIR}
+tar xzf $TON_BINARY_FILE -C ${TON_WORK_DIR}
+
+rm -f $TON_BINARY_FILE
+
 
 exit 0
